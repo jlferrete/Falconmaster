744693dfa7b9dd61aa35f733a8cfa266abd83d0d
Jose Luis Ferrete <joseluisferrete@gmail.com>
2019-01-08 21:02:16 +0100

    Restaurante: Efecto JQuery Header Title

--- restaurante/js/efectos.js
+++ restaurante/js/efectos.js
@@ -10,4 +10,18 @@ $(document).ready(function(){
 			top: '0'
 		}, 2000 + (index * 500));
 	});
+
+// Efecto Header
+	if( $(window).width() > 800 ){
+		$('header .textos').css({
+			opacity: 0,
+			marginTop: 0
+		});
+
+		$('header .textos').animate({
+			opacity: 1,
+			marginTop: '-52px'
+		}, 1500);
+	}
+
 });
