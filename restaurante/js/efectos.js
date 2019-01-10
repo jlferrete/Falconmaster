6e2983c8501729b27e88b2c5f747397b8f91c961
Jose Luis Ferrete <joseluisferrete@gmail.com>
2019-01-10 22:39:40 +0100

    Restaurante: Scroll elementos del menú y efecto parallax

--- restaurante/js/efectos.js
+++ restaurante/js/efectos.js
@@ -24,4 +24,38 @@ $(document).ready(function(){
 		}, 1500);
 	}
 
+	// Scroll Elementos Menu
+	var acercaDe = $('#acerca-de').offset().top,
+		menu = $('#platillos').offset().top,
+		galeria = $('#galeria').offset().top,
+		ubicacion = $('#ubicacion').offset().top;
+
+	$('#btn-acerca-de').on('click', function(e){
+		e.preventDefault();
+		$('html, body').animate({
+			scrollTop: acercaDe - 100
+		}, 500);
+	});
+
+	$('#btn-menu').on('click', function(e){
+		e.preventDefault();
+		$('html, body').animate({
+			scrollTop: menu
+		}, 500);
+	});
+
+	$('#btn-galeria').on('click', function(e){
+		e.preventDefault();
+		$('html, body').animate({
+			scrollTop: galeria
+		}, 500);
+	});
+
+	$('#btn-ubicacion').on('click', function(e){
+		e.preventDefault();
+		$('html, body').animate({
+			scrollTop: ubicacion
+		}, 500);
+	}); 
+
 });
